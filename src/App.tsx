956ce4af3554e014c8e4e429884fa4e74396dbe8
Chilaka Michael Obinna <obichilaka@gmail.com>
2024-11-15 21:07:35 +0100

    setup of vite react

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500">Chucky Soft Solutions Limited</h1>
+      <p>Frontend Test</p>
+    </>
+  );
+};
+
+export default App;
